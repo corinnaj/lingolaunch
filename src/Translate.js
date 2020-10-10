b2d7036138f5ccf63849c546c8d4d16023eb3f3d
Tom Beckmann <tomjonabc@gmail.com>
2020-10-10 23:36:01 +0200

    fixes and improvements to translation ui

--- src/Translate.js
+++ src/Translate.js
@@ -1,6 +1,9 @@
 import {
+  Button,
+  ButtonGroup,
   Checkbox,
   CircularProgress,
+  FilledInput,
   FormControl,
   IconButton,
   Input,
@@ -10,51 +13,101 @@ import {
   ListItem,
   ListItemIcon,
   ListItemText,
+  OutlinedInput,
   TextField,
   Typography,
 } from "@material-ui/core";
-import { Send } from "@material-ui/icons";
+import { PlaylistAdd, PlaylistAddCheck, Send } from "@material-ui/icons";
 import React, { useState } from "react";
 import Camera from "react-html5-camera-photo";
 import "react-html5-camera-photo/build/css/index.css";
-import { dictionary } from "./Dictionary";
+import { translate } from "./Dictionary";
 
 export function Translate() {
   const [loading, setLoading] = useState(false);
   const [text, setText] = useState("");
   const [translatedText, setTranslatedText] = useState("");
   const [textToTranslate, setTextToTranslate] = useState("");
+  const [targetLanguageCode, setTargetLanguageCode] = useState("de");
+  const [wordAdded, setWordAdded] = useState(false);
 
   const translateText = async () => {
     try {
       setLoading(true);
-      setTranslatedText(await dictionary.translate(textToTranslate));
+      setWordAdded(false);
+      setTranslatedText(await translate(textToTranslate, "en"));
     } finally {
       setLoading(false);
     }
   };
 
+  const addWord = () => {
+    setWordAdded(true);
+    // TODO add to dict via corinnas code
+  };
+
   return (
     <div>
-      {translatedText && <>Translated: {translatedText}</>}
-      {loading && (
-        <div>
-          <CircularProgress />
-        </div>
-      )}
-
       <div style={{ padding: "1rem" }}>
-        <FormControl>
-          <InputLabel>Password</InputLabel>
-          <Input
-            label="Enter Text to translate ..."
+        {translatedText && (
+          <>
+            <Typography variant="overline">Translation</Typography>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <Typography variant="h5">{translatedText}</Typography>
+              <div style={{ flexGrow: 1 }}></div>
+              <IconButton disabled={wordAdded} onClick={addWord}>
+                {wordAdded ? <PlaylistAddCheck /> : <PlaylistAdd />}
+              </IconButton>
+            </div>
+          </>
+        )}
+        {loading && (
+          <div>
+            <CircularProgress />
+          </div>
+        )}
+
+        <ButtonGroup
+          color="primary"
+          aria-label="outlined primary button group"
+          style={{ margin: "1rem 0" }}
+        >
+          <Button
+            onClick={() => setTargetLanguageCode("de")}
+            disableElevation
+            variant={targetLanguageCode === "de" ? "contained" : "outlined"}
+          >
+            DE
+          </Button>
+          <Button
+            onClick={() => setTargetLanguageCode("en")}
+            disableElevation
+            variant={targetLanguageCode === "en" ? "contained" : "outlined"}
+          >
+            EN
+          </Button>
+        </ButtonGroup>
+
+        <FormControl fullWidth variant="outlined">
+          <InputLabel htmlFor="text-translate">
+            Enter text to translate ...
+          </InputLabel>
+          <OutlinedInput
+            autoFocus
+            id="text-translate"
             variant="outlined"
+            onKeyPress={(e) => e.key === "Enter" && translateText()}
             fullWidth
             value={textToTranslate}
             onChange={(e) => setTextToTranslate(e.target.value)}
+            labelWidth={180}
             endAdornment={
               <InputAdornment position="end">
-                <IconButton onClick={translateText}>
+                <IconButton
+                  disabled={loading}
+                  onClick={translateText}
+                  edge="end"
+                >
                   <Send />
                 </IconButton>
               </InputAdornment>
