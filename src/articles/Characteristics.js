f1e8420a4eaebb1287cfd07c82cc3c07fff8d4e1
Corinna Jaschek <corinna.jaschek@gmail.com>
2020-10-11 10:47:35 +0200

    more words, sort words by difficulty (kinda)

--- src/articles/Characteristics.js
+++ src/articles/Characteristics.js
@@ -10,15 +10,15 @@ export const Characteristics = () => {
       title="What are the characteristics of German people?"
       image={germanImage}
     >
-      <p>It is true that many Germans tend to place punctuality as a high priority. Hence the global observation that 
-      German trains often run perfectly on time. German people tend to be thrifty, be sensible, and respect one 
-      another’s privacy, and they typically respect the structure and laws of society to an above-average degree. 
+      <p>It is true that many Germans tend to place punctuality as a high priority. Hence the global observation that
+      German trains often run perfectly punctual. German people tend to be thrifty, be sensible, and respect one
+      another’s privacy, and they typically respect the structure and laws of society to an above-average degree.
       There is no place that this sense of ‘order’ is more apparent than in German business culture.</p>
 
-      <p>Traditionally, German people tie a lot of importance to notions of family and community. Regarding the latter, 
-      this is partially where the well-known ‘rule-following’ and orderly nature of the German people comes from: if 
-      everyone in the community follows the rules and does things the right way, the neighborhood/town/city/country 
-      will be a nice place for all residents to live, hence why many might seem like sticklers when it comes to correct 
+      <p>Traditionally, German people tie a lot of importance to notions of family and community. Regarding the latter,
+      this is partially where the well-known ‘rule-following’ and orderly nature of the German people comes from: if
+      everyone in the community follows the rules and does things the right way, the neighborhood/town/city/country
+      will be a nice place for all residents to live, hence why many might seem like sticklers when it comes to correct
       recycling and late-night noise - they take it seriously for the sake of everyone.</p>
     </Article>
   );
