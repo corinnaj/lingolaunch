bb574a6bd443be5310468da9f33314715c126d4b
Wanda Baltzer <wanda.baltzer@student.hpi.de>
2020-10-11 10:46:37 +0200

    show new words in the vocab trainer

--- src/IndexCard.js
+++ src/IndexCard.js
@@ -1,13 +1,17 @@
 import React, { useContext, useState } from "react";
 import { Dictionary } from "./Dictionary.js";
-import { Typography, Divider, Card, CardContent, ListItemText, ListItem, List } from "@material-ui/core";
+import { Typography, Fab, Card, CardContent, ListItemText} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
 
 const useStyles = makeStyles((theme) => ({
   root: {
     padding: theme.spacing(1),
     margin: "1rem",
+  },
+  card: {
+    height: 100,
   },
 }));
 
@@ -20,6 +24,7 @@ export function IndexCard() {
     var keys = Object.keys(knownWords);
     var randomKey = Math.floor((Math.random() * keys.length));
     var word = keys[randomKey]
+    if (!dictionary[word]) word = word.charAt(0).toUpperCase() + word.slice(1)
     return word
   };
 
@@ -28,21 +33,41 @@ export function IndexCard() {
     setRevealedWord(translation)
   }
 
-  const [randomWord] = useState(getRandomWord()) 
+  function showNewWord() {
+    setRandomWord(getRandomWord)
+    setRevealedWord(null)
+  }
+
+  const [randomWord, setRandomWord] = useState(getRandomWord()) 
   const [revealedWord, setRevealedWord] = useState(null)
 
   return (
     <div className={classes.root}>
       <Typography>Study your vocab! Guess what the word means and check your answer by clicking on the card.</Typography>
+      
       <br />
+
       <div onClick={() => {showTranslation(randomWord)}}>
-        <Card elevation={4}>
+        <Card className={classes.card} elevation={4}>
           <CardContent>
-            <ListItemText primary={randomWord} secondary={revealedWord}/> 
+            <ListItemText align="center" primary={randomWord} secondary={revealedWord}/> 
           </CardContent>
         </Card>
       </div>
 
+      <br /> 
+
+      <div align="right">
+        <Fab 
+          color="primary" 
+          variant="extended"
+          onClick={() => {showNewWord()}}
+        >
+          <ArrowForwardIosIcon />
+          Next Card
+        </Fab>
+      </div>
+  
     </div>
   )
 }
