e58376149ca072bfbd7598e8322ab09be0f8b7ec
Tom Beckmann <tomjonabc@gmail.com>
2020-10-11 17:09:43 +0200

    disable phoneframe by default

--- src/ScrollToTop.js
+++ src/ScrollToTop.js
@@ -2,12 +2,15 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 export default function ScrollToTop() {
-    const { pathname } = useLocation();
+  const { pathname } = useLocation();
 
-    useEffect(() => {
-        document.getElementsByClassName("phone-wrapper")[0].scrollTo(0, 0);
-        window.scrollTo(0, 0);
-    }, [pathname]);
+  useEffect(() => {
+    (document.getElementsByClassName("phone-wrapper")[0] || window).scrollTo(
+      0,
+      0
+    );
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
-    return null;
+  return null;
 }
